032e710c66f6a24ce5c2d2ebd8480a9b340f5b93
Charles Erickson <cse621@gmail.com>
2019-04-04 20:00:50 -0500

    added list to inventory that holds stock of lemons, cups, sugar, and ice

--- LemonadeStand/Inventory.cs
+++ LemonadeStand/Inventory.cs
@@ -8,23 +8,33 @@ namespace LemonadeStand
     public class Inventory
     {
         public double StartingMoney = 20.00;
-        public List<int> supplies;
         public Store store;
         public Lemons lemons;
         public Sugar sugar;
         public Ice ice;
         public Cups cups;
+        public List<int> Supplies;
         //methods
 
        
 
         public Inventory()
         {
-            supplies = new List<int>
+
+            lemons = new Lemons();
+            sugar = new Sugar();
+            ice = new Ice();
+            cups = new Cups();
+            store = new Store();
+            Supplies = new List<int>;
+            void AddSupplies()
             {
+                Supplies.Add(lemons.ItemCount);
+                Supplies.Add(sugar.ItemCount);
+                Supplies.Add(ice.ItemCount);
+                Supplies.Add(cups.ItemCount);
+            }
 
-
-            };
         }
 
     }    
